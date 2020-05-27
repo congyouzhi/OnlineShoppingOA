40a6f7ee9f122da03fa55dc6e56faf656e0444a6
cong.yzh <cong.yzh@neusoft.com>
2020-05-27 17:34:07 +0800

    author:【丛有志】 task:【权限管理功能-列表查询】

--- vue_shop/src/plugins/element.js
+++ vue_shop/src/plugins/element.js
@@ -25,7 +25,8 @@ import {
   Tooltip,
   Pagination,
   Dialog,
-  MessageBox
+  MessageBox,
+  Tag
 } from 'element-ui'
 
 
@@ -51,6 +52,7 @@ Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
+Vue.use(Tag)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
 
