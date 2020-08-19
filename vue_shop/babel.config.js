7b29538ecaab03898d9c383bbf43ff21a0b87bb0
cong.yzh <cong.yzh@neusoft.com>
2020-08-19 23:30:10 +0800

    【丛有志提交】203项目优化,修改成CDN

--- vue_shop/babel.config.js
+++ vue_shop/babel.config.js
@@ -1,3 +1,8 @@
+// 项目发布阶段需要用到的label插件
+const proPlugins = []
+if ('production'===process.env.NODE_ENV) {
+  proPlugins.push('transform-remove-console')
+}
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
@@ -9,6 +14,8 @@ module.exports = {
         libraryName: 'element-ui',
         styleLibraryName: 'theme-chalk'
       }
-    ]
+    ],
+    // 发布产品时候的插件数组
+    ...proPlugins
   ]
 }
--- vue_shop/vue.config.js
+++ vue_shop/vue.config.js
@@ -1,3 +1,30 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  /*先进入默认的打包入口[main.js][app], 清理一下，将自己的添加进入*/
+  // 发布模式
+  chainWebpack: config => {
+    config.when(process.env.NODE_ENV ==='production',config=>{
+      config.entry('app').clear().add('./src/main-prod.js')
+
+      config.set('externals', {
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        axios: 'axios',
+        lodash: '_',
+        echarts: 'echarts',
+        nprogress: 'NProgress',
+        'vue-quill-editor': 'VueQuillEditor'
+      })
+
+      config.plugin('html').tap(args => {
+        args[0].isProd = true
+        return args
+      })
+    })
+
+    // 开发模式
+    config.when(process.env.NODE_ENV ==='development',config=>{
+      config.entry('app').clear().add('./src/main-dev.js')
+    })
+  }
 }
